10729dcb1e62d956671fc81245eb30cfd9ca1c9d
Lucas Caton <lucascaton@gmail.com>
2014-09-10 17:02:32 +1000

    Creating 'Seek:Enumerations' class

--- lib/seek.rb
+++ lib/seek.rb
@@ -2,6 +2,8 @@ require 'seek/version'
 require 'seek/adapters/soap_client'
 require 'seek/integrations/base'
 require 'seek/integrations/fastlane_plus'
+require 'seek/enumerations'
+Dir["#{File.expand_path('..', __FILE__)}/seek/enumerations/*.rb"].each { |f| require f }
 
 module Seek
 end
---
+++ lib/seek/enumerations.rb
@@ -0,0 +1,36 @@
+require 'ostruct'
+
+module Seek
+  class Enumerations
+    class << self
+      Dir["#{File.expand_path('..', __FILE__)}/enumerations/*.yml"].each do |file|
+        resource = File.basename(file, '.yml').to_sym
+
+        send :define_method, resource, ->{ instance_variable_get("@#{resource}") || instance_variable_set("@#{resource}", create_hash_from_yml(resource)) }
+      end
+
+      private
+
+      def create_hash_from_yml(resource)
+        file = File.expand_path("../enumerations/#{resource}.yml", __FILE__)
+        hashes2ostruct(YAML.load_file(file)['enumerations'][resource.to_sym])
+      end
+
+      def hashes2ostruct(object)
+        case object
+          when Hash
+            object = object.clone
+            object.each do |key, value|
+              object[key] = hashes2ostruct(value)
+            end
+            OpenStruct.new(object)
+          when Array
+            object = object.clone
+            object.map! { |i| hashes2ostruct(i) }
+          else
+            object
+        end
+      end
+    end
+  end
+end
