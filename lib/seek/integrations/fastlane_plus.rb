43547886e60a16a81709e798304e98e195c78a52
Lucas Caton <lucascaton@gmail.com>
2014-09-10 10:30:18 +1000

    Using Nokogiri to build XMLs

--- lib/seek/integrations/fastlane_plus.rb
+++ lib/seek/integrations/fastlane_plus.rb
@@ -15,61 +15,69 @@ module Seek
       end
 
       def build_envelope
-        xml_content = <<-XML
-          <?xml version="1.0" encoding="utf-8"?>
-          <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-            <soap:Body>
-              <UploadFile xmlns="http://webservices.seek.com.au">
-                <Token>#{encrypted_access_token}</Token>
+        Nokogiri::XML::Builder.new(encoding: 'utf-8') do |xml|
+          xml.send 'soap:Envelope',
+            'xmlns:xsi'  => 'http://www.w3.org/2001/XMLSchema-instance',
+            'xmlns:xsd'  => 'http://www.w3.org/2001/XMLSchema',
+            'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/' do
 
-                <xmlFastlaneFile>
-                  <FastLanePlus UploaderID="#{@uploader_id}" AgentID="" Version="3.0">
-                    <Client ID="#{@client_id}" MinJobs="0" MaxJobs="9999999">
-                      #{jobs.join("\n")}
-                    </Client>
-                  </FastLanePlus>
-                </xmlFastlaneFile>
-              </UploadFile>
-            </soap:Body>
-          </soap:Envelope>
-        XML
+            xml.send 'soap:Body' do
+              xml.send 'UploadFile', xmlns: 'http://webservices.seek.com.au' do
+                xml.send 'Token', 'encrypted_access_token'
 
-        xml_content.strip
+                xml.send 'xmlFastlaneFile' do
+                  xml.send 'FastLanePlus', 'UploaderID' => @uploader_id, 'AgentID' => '', 'Version' => '3.0' do
+                    xml.send 'Client', 'ID' => @client_id, 'MinJobs' => '0', 'MaxJobs' => '9999999' do
+                      xml << jobs.join("\n")
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end.to_xml
       end
 
       def jobs
-        job_content = <<-JOB
-        <Job Reference="TEST1" TemplateID="" ScreenID="">
-          <Title>Test Job Ad – Please Do Not Apply</Title>
-          <SearchTitle>Test Job Ad – Please Do Not Apply</SearchTitle>
-          <Description>Test Job Ad – Please Do Not Apply</Description>
-          <AdDetails><![CDATA[\<br\>test \<b\>This line is bold\</b\>]]></AdDetails>
-          <ApplicationEmail>person@xyzcompany.com.au</ApplicationEmail>
-          <ApplicationURL></ApplicationURL>
-          <ResidentsOnly>Yes</ResidentsOnly>
-          <Items>
-            <Item Name="Jobtitle">Graduate Architect (test)</Item>
-            <Item Name="Bullet1">-</Item>
-            <Item Name="Bullet2">-</Item>
-            <Item Name="Bullet3">-</Item>
-            <Item Name="ContactName">-</Item>
-            <Item Name="PhoneNo">-</Item>
-            <Item Name="FaxNo">-</Item>
-            <Item Name="RefNumber">SK/CC1234</Item>
-          </Items>
-          <Listing MarketSegments="Main Campus">
-            <Classification Name="Location">Sydney</Classification>
-            <Classification Name="Area">CBDInnerWestEasternSuburbs</Classification>
-            <Classification Name="WorkType">FullTime</Classification>
-            <Classification Name="Classification">DesignArchitecture</Classification>
-            <Classification Name="SubClassification">Architecture</Classification>
-          </Listing>
-          <Salary Type="AnnualPackage" Min="50000" Max="69999" AdditionalText="" />
-          <StandOut IsStandOut="false" LogoID="" Bullet1="" Bullet2="" Bullet3="" />
-        </Job>
-        JOB
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml.send 'Job', 'Reference' => 'TEST1', 'TemplateID' => '', 'ScreenID' => '' do
+            xml.send 'Title',       'Test Job Ad – Please Do Not Apply'
+            xml.send 'SearchTitle', 'Test Job Ad – Please Do Not Apply'
+            xml.send 'Description', 'Test Job Ad – Please Do Not Apply'
 
-        [job_content.strip]
+            xml.send 'AdDetails' do
+              xml.cdata('<br \>test <b>This line is bold</b>')
+            end
+
+            xml.send 'ApplicationEmail', 'person@xyzcompany.com.au'
+            xml.send 'ApplicationURL', ''
+            xml.send 'ResidentsOnly', 'Yes'
+
+            xml.send 'Items' do
+              xml.send 'Item', 'Test Job Ad – Please Do Not Apply', 'Name' => 'Jobtitle'
+              xml.send 'Item', '-',                                 'Name' => 'Bullet1'
+              xml.send 'Item', '-',                                 'Name' => 'Bullet2'
+              xml.send 'Item', '-',                                 'Name' => 'Bullet3'
+              xml.send 'Item', '-',                                 'Name' => 'ContactName'
+              xml.send 'Item', '-',                                 'Name' => 'PhoneNo'
+              xml.send 'Item', '-',                                 'Name' => 'FaxNo'
+              xml.send 'Item', 'SK/CC1234',                         'Name' => 'RefNumber'
+            end
+
+            xml.send 'Listing', 'MarketSegments' => 'Main Campus' do
+              xml.send 'Classification', 'Sydney',                     'Name' => 'Location'
+              xml.send 'Classification', 'CBDInnerWestEasternSuburbs', 'Name' => 'Area'
+              xml.send 'Classification', 'FullTime',                   'Name' => 'WorkType'
+              xml.send 'Classification', 'DesignArchitecture',         'Name' => 'Classification'
+              xml.send 'Classification', 'Architecture',               'Name' => 'SubClassification'
+            end
+
+            xml.send 'Salary', 'Type' => 'AnnualPackage', 'Min' => '50000', 'Max' => '69999', 'AdditionalText' => ''
+            xml.send 'StandOut', 'IsStandOut' => 'false', 'LogoID' => '', 'Bullet1' => '', 'Bullet2' => '', 'Bullet3' => ''
+          end
+        end
+
+        [Nokogiri::XML(builder.to_xml).root.to_xml]
       end
 
       private
