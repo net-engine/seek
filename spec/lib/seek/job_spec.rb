349684accb8557beecb086bfb7602fef45eda55e
Lucas Caton <lucascaton@gmail.com>
2014-09-16 11:50:19 +1000

    Improving tests

--- spec/lib/seek/job_spec.rb
+++ spec/lib/seek/job_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Seek::Job do
   it 'assigns instance variables' do
-    attribute_1, attribute_2 = Seek::Job::REQUIRED_ATTRIBUTES.sample(2)
-    attribute_3, attribute_4 = Seek::Job::NOT_REQUIRED_ATTRIBUTES.sample(2)
+    attribute_1, attribute_2 = described_class::REQUIRED_ATTRIBUTES.sample(2)
+    attribute_3, attribute_4 = described_class::NOT_REQUIRED_ATTRIBUTES.sample(2)
 
     subject = described_class.new(attribute_1 => 'A', attribute_2 => 'B')
 
@@ -14,5 +14,29 @@ RSpec.describe Seek::Job do
     expect(subject.send(attribute_2)).to eql('B')
     expect(subject.send(attribute_3)).to eql('C')
     expect(subject.send(attribute_4)).to eql('D')
+  end
+
+  describe '#valid?' do
+    subject { described_class.new({}) }
+
+    context 'when it has all required attributes' do
+      it 'returns true' do
+        described_class::REQUIRED_ATTRIBUTES.each do |attribute|
+          subject.send("#{attribute}=", 'value')
+        end
+
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when it does not have all required attributes' do
+      it 'returns false' do
+        described_class::REQUIRED_ATTRIBUTES.drop(1).each do |attribute|
+          subject.send("#{attribute}=", 'value')
+        end
+
+        expect(subject).to_not be_valid
+      end
+    end
   end
 end
