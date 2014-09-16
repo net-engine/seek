edec2234a58819ef566ccf0b6b7f502476f91971
Lucas Caton <lucascaton@gmail.com>
2014-09-16 11:50:58 +1000

    Updating required and not required attributes in 'Job' class

--- lib/seek/job.rb
+++ lib/seek/job.rb
@@ -1,14 +1,19 @@
 module Seek
   class Job
     REQUIRED_ATTRIBUTES = %i[
-      reference template_id screen_id title search_title description ad_details application_email application_url residents_only
-      job_title bullet1 bullet2 bullet3 contact_name phone_number fax_number reference_number
-      location area work_type classification sub_classification
-      salary_type salary_min salary_max salary_additional_text
-      is_stand_out stand_out_logo_id stand_out_bullet1
+      reference title search_title description ad_details
+      listing_market_segments listing_location listing_work_type listing_classification listing_sub_classification
+      salary_type salary_min salary_max
+      is_stand_out
     ]
 
-    NOT_REQUIRED_ATTRIBUTES = %i[stand_out_bullet2 stand_out_bullet3]
+    NOT_REQUIRED_ATTRIBUTES = %i[
+      template_id screen_id application_email application_url residents_only
+      item_job_title item_bullet1 item_bullet2 item_bullet3 item_contact_name item_phone_number item_fax_number item_reference_number
+      listing_area
+      salary_additional_text
+      stand_out_logo_id stand_out_bullet1 stand_out_bullet2 stand_out_bullet3
+    ]
 
     attr_accessor(*REQUIRED_ATTRIBUTES)
     attr_accessor(*NOT_REQUIRED_ATTRIBUTES)
