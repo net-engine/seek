7231828f8fa4ed52d977bf2771af9db180d37b79
Lucas Caton <lucascaton@gmail.com>
2014-11-10 11:54:20 +1000

    Adding hourly rate methods to SalaryRanges class

--- spec/lib/seek/salary_ranges_spec.rb
+++ spec/lib/seek/salary_ranges_spec.rb
@@ -7,10 +7,23 @@ RSpec.describe Seek::SalaryRanges do
     end
   end
 
+  describe '.hourly_rate_values' do
+    it 'returns a non-empty array' do
+      expect(described_class.hourly_rate_values.size).to be > 1
+    end
+  end
+
   describe '.available_max_values' do
     it 'returns the correct array' do
       expect(described_class.available_max_values(0)).to     eql([19999, 24999, 29999, 34999, 39999])
       expect(described_class.available_max_values(60000)).to eql([64999, 69999, 74999, 79999])
+    end
+  end
+
+  describe '.hourly_rate_max_values' do
+    it 'returns the correct array' do
+      expect(described_class.hourly_rate_max_values(0)).to  eql([9.99, 14.99, 19.99])
+      expect(described_class.hourly_rate_max_values(50)).to eql([54.99, 59.99, 64.99, 69.99, 74.99])
     end
   end
 end
